0fa32a2a5b801a2ca81c7213557e781cef6b5276
Domokos Benedek <benedekdomokos50@gmail.com>
2024-12-03 12:45:36 +0100

    Kifagyas megoldva

--- app/src/main/java/com/example/android_konyvtar/details_activity.java
+++ app/src/main/java/com/example/android_konyvtar/details_activity.java
@@ -24,7 +24,7 @@ public class details_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.details_activity);
+        setContentView((R.layout.details_activity));
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
